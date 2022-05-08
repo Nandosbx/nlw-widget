ddec9ddb7cafe31f6189e0c478c89cea1a282c88
Fernando <nandosbx@users.noreply.github.com>
2022-05-08 01:57:14 -0300

    Phosphor UI & Headless UI

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,11 +1,19 @@
+import { Popover } from "@headlessui/react";
 import { ChatTeardropDots } from "phosphor-react";
 
 export function Widget() {
   return (
-    <div className='absolute bottom-5 right-5'>
-      <button className='bg-brand-500 rounded-full px-3 h-12 text-white'>
+    <Popover className='absolute bottom-5 right-5'>
+      <Popover.Panel>Hello</Popover.Panel>
+
+      <Popover.Button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
         <ChatTeardropDots className='w-6 h-6' />
-      </button>
-    </div>
+
+        <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
+          <span className='pl-2'></span>
+          Feedback
+        </span>
+      </Popover.Button>
+    </Popover>
   );
 }
